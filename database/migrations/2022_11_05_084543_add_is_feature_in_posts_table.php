788ce3c052c0ef639f67c43860255aa4cbe9280a
李晏慈 <3a832005@gm.student.ncut.edu.tw>
2022-12-05 23:01:35 +0800

    修正:新增down()方法

--- database/migrations/2022_11_05_084543_add_is_feature_in_posts_table.php
+++ database/migrations/2022_11_05_084543_add_is_feature_in_posts_table.php
@@ -27,7 +27,8 @@ return new class extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->string('title')->change();
+            $table->dropColumn('is_feature');
         });
     }
 };
